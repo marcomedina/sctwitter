6133039089b72fb0bc98004547ce6d65ae66538f
Marco Medina <marco@wizeline.com>
2015-06-21 22:48:06 -0500

    1st Commit, Includes Auth and UI

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :twitter_username, presence: true
+end
